7f36febf6168acbf12cd4c009af7770d6ebc51af
krishna bashyal <krishna.hello23@gmail.com>
2022-12-27 10:35:25 +0545

    operators assignment

---
+++ opeator.py
@@ -0,0 +1,17 @@
+# Write a python script to check weather the number is even or odd and find their range.
+
+a= 10
+b=500
+# user input??
+
+if a%2==0:
+    print(' the number ',a," is even")
+else:
+    print(a,' is odd ')
+
+#now for range
+if a>b:
+    print('the range of ',a,' and ',b,' is ', a-b )
+else:
+    print('the range of ',a,' and ',b,' is ', b-a )
+    
