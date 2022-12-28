6b90369e63cc8e0d81be57691d1ad46643a68a9a
krishna bashyal <krishna.hello23@gmail.com>
2022-12-28 17:50:01 +0545

    closest to 0

---
+++ closest.py
@@ -0,0 +1,12 @@
+#wap to find the number closest to 0
+
+x,y,z = 321535,54654,2594
+if x<y and x<z:
+     print ('the number closest to 0 is ',x)
+elif y<z:
+    print('the number closest to 0 is ', y)
+else:
+    print('the  number closest to 0 is ', z)
+
+
+
